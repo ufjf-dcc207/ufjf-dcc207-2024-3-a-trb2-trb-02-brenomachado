5a7def79ed60ba148439f3ae6d7e68a1aa202b49
Breno Machado <burracha.6464@gmail.com>
2025-02-09 23:39:36 -0300

    Aplica state e renderiza itens

--- src/Componentes/Deque.tsx
+++ src/Componentes/Deque.tsx
@@ -1,13 +1,17 @@
+import { useState } from 'react'
 import './Deque.css'
 
 export default function Deque()
 {
- 
+    const [itens, setItens] = useState<number[]>([]);
+
     return (
         <div className='Geral'>
             <h1>Fila Dupla</h1>
             <div className='FilaDupla'>
-
+                { itens.map((item) => (
+                    <div className='Elemento'>{item}</div>
+                ))}
             </div>
             <div className='Botoes'>
                 
