6b1f2403c678f29440b10d8f77429897ae81bb3d
Breno Machado <burracha.6464@gmail.com>
2025-02-09 23:36:07 -0300

    Cria interface do deque

--- src/Componentes/Deque.tsx
+++ src/Componentes/Deque.tsx
@@ -2,9 +2,37 @@ import './Deque.css'
 
 export default function Deque()
 {
+ 
     return (
         <div className='Geral'>
-            <h1>Fila Dupla / Deque </h1>
+            <h1>Fila Dupla</h1>
+            <div className='FilaDupla'>
+
+            </div>
+            <div className='Botoes'>
+                
+                <input 
+                    type="number" 
+                    placeholder="numero"
+                />
+                <section>
+                    <button onClick={
+                        ()=>{}
+                    }>Enfileira Frente</button>
+                    <button onClick={
+                        ()=>{}
+                    }>Enfileira Atrás</button>
+                    <button onClick={
+                        ()=>{}
+                    }>Desenfileira Atrás</button>
+                    <button onClick={
+                        () => {}
+                    }>Desenfileira Frente</button>
+                </section>
+               
+
+            </div>
+
         </div>
     )
 }
